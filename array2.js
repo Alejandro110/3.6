fed935b43077b5e794c6cf43584dda38d8d18d3b
2j8e <2j8e@fresnou.org>
2023-03-29 10:28:53 -0700

    initial commit

---
+++ array2.js
@@ -0,0 +1,15 @@
+function countup(x) {
+    if (x <= 0) {
+        console.log('invalid');
+    } else {
+        let newarray = []
+        for (let i = 1; i <= x; i++)
+         for   (let i = x;i<=x;i++)
+        {
+            newarray.push(x)
+        }
+        console.log(newarray);
+    }
+}
+
+countup(17)
--- arraynotes.js
+++ arraynotes.js
@@ -1,7 +1,15 @@
+//
 function firstelement(arr){
     console.log(arr[6]);
 }
 let myNums=[1,2,3,4];
 firstelement(myNums);
 
-let students =["broke array"]
+
+//2
+
+function printSecond(arr){
+
+    console.log( arr[] )
+    console.log(students[array]);
+}
